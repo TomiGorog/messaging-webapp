25cb21084cec3fd1191aa91b9d1700d76195d21a
TomiGorog <tamas.peter.gorog@gmail.com>
2023-04-07 16:58:29 +0200

    needed to add keywords to cards for image search

--- messaging-webapp/src/components/SavedNewsCard.tsx
+++ messaging-webapp/src/components/SavedNewsCard.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { AuthContext } from '../contexts/authContext';
 import { Card, Link } from '@mui/material';
 import Grid from '@mui/material/Grid';
@@ -8,7 +8,6 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { createTheme } from "@mui/material"
-import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
 import ShareIcon from '@mui/icons-material/ShareOutlined';
 import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 const theme = createTheme({
@@ -31,24 +30,32 @@ const SavedNewsCard = ({
 }: newsCardProps) => {
 
     const { deleteFromSavedArticles, userId } = useContext(AuthContext)
-
+    useEffect(() => {
+        if (image == null || image == undefined || image == "") {
+            fetch(`https://pixabay.com/api/?key=${import.meta.env.VITE_PIXABAY_API_KEY}&q=query`)
+                .then(res => res.json())
+                .then(res => console.log(res))
+        }
+    },
+        [])
     return (
         <Grid item xs={3}>
-            <Card sx={{ maxWidth: 380, minHeight: 600, maxHeight: 600, border: 2 }}>
+            <Card sx={{ maxWidth: 400, minHeight: 600, maxHeight: 600, border: 2 }}>
                 <CardMedia sx={{ height: 140 }} image={image} title={title}
                 >  </CardMedia>
-                <CardContent sx={{ height: 350, marginBottom: 2 }}>
+                <CardContent sx={{ height: 350, marginBottom: 4, overflow: 'hidden' }}>
                     <Typography gutterBottom variant="h6" component="h3">
                         {title}
                     </Typography>
                     <Typography sx={{ overflow: 'hidden', height: 200 }} variant="body2" color="text.secondary">
-
                         {description}
                     </Typography>
                 </CardContent>
-                <CardActions>
-                    <a href={link} target="_blank" >Read more</a>
-
+                <CardActions sx={{
+                    display: 'flex',
+                    justifyContent: 'space-evenly',
+                }}>
+                    <Link href={link} target="_blank" >Read more</Link>
                     <Link component="button" href={link}
                         variant="body2" underline='hover' target="_blank" color="primary" rel="noreferrer noreferrer"
                     ></Link>
