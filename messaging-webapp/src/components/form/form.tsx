9b3d3734e3b859d7f28f169ea2ed33c249959368
TomiGorog <tamas.peter.gorog@gmail.com>
2023-04-19 19:19:15 +0200

    getting to know react hook form form validation

--- messaging-webapp/src/components/form/form.tsx
+++ messaging-webapp/src/components/form/form.tsx
@@ -1,15 +1,21 @@
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 
 
 const form = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    console.log(errors)
+    const { register, handleSubmit, watch, formState: { errors } } = useForm({
+        defaultValues: {
+            firstName: "",
+            lastName: ""
+        }, mode: "onBlur"
+    });
     return (
         <form onSubmit={handleSubmit((data) => {
             console.log(data)
         })}>
             <input {...register("firstName", { required: "This field is mandatory", minLength: { value: 2, message: "Minimum 2 characters" } })} placeholder="First Name" />
-            {errors.firstName && <span>This field is required</span>}
+            {errors.firstName && errors.firstName.type === "required" && <span>{errors.firstName.message}</span>}
+            {errors.firstName && errors.firstName.type === "minLength" && (<span>{errors.firstName.message}</span>)}
             <input {...register("lastName", { required: "This field is mandatory", minLength: { value: 4, message: "Minimum 4 characters" } })} placeholder="Last Name" />
             <button type="submit">Send</button>
         </form>
